37be6f7cefce6bd90b7d01787519d832a6114f28
kinkl <montecarlo507@gmail.com>
2019-07-27 15:20:30 +0500

    Initial commit

---
+++ src/main/java/com/kinkl/paymentlistservice/PaymentListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kinkl.paymentlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentListServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinkl/paymentlistservice/PaymentListServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kinkl.paymentlistservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaymentListServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
