f23e926ebdf6be656e7ed00e477344fd27e9e951
kinkl <montecarlo507@gmail.com>
2019-07-28 20:06:05 +0500

    Make service perform batch updates asynchronously for different databases

--- src/main/java/com/kinkl/paymentlistservice/service/PaymentService.java
+++ src/main/java/com/kinkl/paymentlistservice/service/PaymentService.java
@@ -4,44 +4,56 @@ import com.kinkl.paymentlistservice.domain.Payment;
 import com.kinkl.paymentlistservice.dto.PaymentDto;
 import com.kinkl.paymentlistservice.dto.SenderTotalInfoDto;
 import com.kinkl.paymentlistservice.repository.PaymentRepository;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 @Service
-public class PaymentService {
+public class PaymentService implements DisposableBean {
 
     @Autowired
     @Qualifier(value = "db0PaymentRepository")
     private PaymentRepository db0PaymentRepository;
 
+    private final ExecutorService db0InsertPaymentExecutor = Executors.newSingleThreadExecutor();
+
     @Autowired
     @Qualifier(value = "db1PaymentRepository")
     private PaymentRepository db1PaymentRepository;
+
+    private final ExecutorService db1InsertPaymentExecutor = Executors.newSingleThreadExecutor();
 
     @Autowired
     @Qualifier(value = "db2PaymentRepository")
     private PaymentRepository db2PaymentRepository;
 
+    private final ExecutorService db2InsertPaymentExecutor = Executors.newSingleThreadExecutor();
+
     public List<PaymentDto> savePayments(List<PaymentDto> payments) {
-        List<PaymentDto> unhandledPayments = new ArrayList<>();
         Map<Integer, List<PaymentDto>> groupsOfPayments = payments.stream()
                 .collect(Collectors.groupingBy(e -> getDatabaseNodeNumberForSenderId(e.getSenderId())));
+        List<Future<List<PaymentDto>>> futures = new ArrayList<>();
         for (Map.Entry<Integer, List<PaymentDto>> entry : groupsOfPayments.entrySet()) {
-            PaymentRepository paymentRepository = getPaymentRepository(entry.getKey());
-            List<PaymentDto> paymentListToSaveDto = entry.getValue();
-            List<Payment> paymentListToSave = paymentListToSaveDto.stream() //
-                    .map(payment -> new Payment(payment.getSenderId(), payment.getRecipientId(), payment.getAmount())) //
-                    .collect(Collectors.toList());
+            Future<List<PaymentDto>> future = submitPaymentListInsertion(entry);
+            futures.add(future);
+        }
+        List<PaymentDto> unhandledPayments = new ArrayList<>();
+        for (Future<List<PaymentDto>> future : futures) {
             try {
-                paymentRepository.saveAll(paymentListToSave);
-            } catch (Exception e) {
-                unhandledPayments.addAll(paymentListToSaveDto);
+                unhandledPayments.addAll(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
         }
         return unhandledPayments;
@@ -53,8 +65,35 @@ public class PaymentService {
         return new SenderTotalInfoDto(senderId, sum);
     }
 
+    private Future<List<PaymentDto>> submitPaymentListInsertion(Map.Entry<Integer, List<PaymentDto>> entry) {
+        int databaseNodeNumber = entry.getKey();
+        ExecutorService insertPaymentExecutor = getInsertPaymentExecutor(databaseNodeNumber);
+        return insertPaymentExecutor.submit(() -> {
+            PaymentRepository paymentRepository = getPaymentRepository(databaseNodeNumber);
+            List<PaymentDto> paymentListToSaveDto = entry.getValue();
+            List<Payment> paymentListToSave = paymentListToSaveDto.stream() //
+                    .map(payment -> new Payment(payment.getSenderId(), payment.getRecipientId(), payment.getAmount())) //
+                    .collect(Collectors.toList());
+            try {
+                paymentRepository.saveAll(paymentListToSave);
+            } catch (Exception e) {
+                return paymentListToSaveDto;
+            }
+            return Collections.emptyList();
+        });
+    }
+
     private int getDatabaseNodeNumberForSenderId(long senderId) {
         return (int) senderId % 3;
+    }
+
+    private ExecutorService getInsertPaymentExecutor(int databaseNodeNumber) {
+        if (databaseNodeNumber == 0) {
+            return this.db0InsertPaymentExecutor;
+        } else if (databaseNodeNumber == 1) {
+            return this.db1InsertPaymentExecutor;
+        }
+        return this.db2InsertPaymentExecutor;
     }
 
     private PaymentRepository getPaymentRepository(int databaseNodeNumber) {
@@ -64,5 +103,12 @@ public class PaymentService {
             return this.db1PaymentRepository;
         }
         return this.db2PaymentRepository;
+    }
+
+    @Override
+    public void destroy() {
+        this.db0InsertPaymentExecutor.shutdown();
+        this.db1InsertPaymentExecutor.shutdown();
+        this.db2InsertPaymentExecutor.shutdown();
     }
 }
