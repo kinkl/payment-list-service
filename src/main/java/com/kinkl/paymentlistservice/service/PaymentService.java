d1d4f3bb7e1f8c2c4647038d689c5fdad3fd2000
Kirill Kin <montecarlo507@gmail.com>
2019-07-29 00:01:19 +0500

    Make service use cached thread pool instead of single thread executors

--- src/main/java/com/kinkl/paymentlistservice/service/PaymentService.java
+++ src/main/java/com/kinkl/paymentlistservice/service/PaymentService.java
@@ -22,30 +22,26 @@ import java.util.stream.Collectors;
 @Service
 public class PaymentService implements DisposableBean {
 
+    private final ExecutorService threadPool = Executors.newCachedThreadPool();
+
     @Autowired
     @Qualifier(value = "db0PaymentRepository")
     private PaymentRepository db0PaymentRepository;
-
-    private final ExecutorService db0InsertPaymentExecutor = Executors.newSingleThreadExecutor();
 
     @Autowired
     @Qualifier(value = "db1PaymentRepository")
     private PaymentRepository db1PaymentRepository;
 
-    private final ExecutorService db1InsertPaymentExecutor = Executors.newSingleThreadExecutor();
-
     @Autowired
     @Qualifier(value = "db2PaymentRepository")
     private PaymentRepository db2PaymentRepository;
-
-    private final ExecutorService db2InsertPaymentExecutor = Executors.newSingleThreadExecutor();
 
     public List<PaymentDto> savePayments(List<PaymentDto> payments) {
         Map<Integer, List<PaymentDto>> groupsOfPayments = payments.stream()
                 .collect(Collectors.groupingBy(e -> getDatabaseNodeNumberForSenderId(e.getSenderId())));
         List<Future<List<PaymentDto>>> futures = new ArrayList<>();
         for (Map.Entry<Integer, List<PaymentDto>> entry : groupsOfPayments.entrySet()) {
-            Future<List<PaymentDto>> future = submitPaymentListInsertion(entry);
+            Future<List<PaymentDto>> future = submitPaymentListInsertion(entry.getKey(), entry.getValue());
             futures.add(future);
         }
         List<PaymentDto> unhandledPayments = new ArrayList<>();
@@ -65,19 +61,16 @@ public class PaymentService implements DisposableBean {
         return new SenderTotalInfoDto(senderId, sum);
     }
 
-    private Future<List<PaymentDto>> submitPaymentListInsertion(Map.Entry<Integer, List<PaymentDto>> entry) {
-        int databaseNodeNumber = entry.getKey();
-        ExecutorService insertPaymentExecutor = getInsertPaymentExecutor(databaseNodeNumber);
-        return insertPaymentExecutor.submit(() -> {
+    private Future<List<PaymentDto>> submitPaymentListInsertion(Integer databaseNodeNumber, List<PaymentDto> payments) {
+        return this.threadPool.submit(() -> {
             PaymentRepository paymentRepository = getPaymentRepository(databaseNodeNumber);
-            List<PaymentDto> paymentListToSaveDto = entry.getValue();
-            List<Payment> paymentListToSave = paymentListToSaveDto.stream() //
+            List<Payment> paymentListToSave = payments.stream() //
                     .map(payment -> new Payment(payment.getSenderId(), payment.getRecipientId(), payment.getAmount())) //
                     .collect(Collectors.toList());
             try {
                 paymentRepository.saveAll(paymentListToSave);
             } catch (Exception e) {
-                return paymentListToSaveDto;
+                return payments;
             }
             return Collections.emptyList();
         });
@@ -85,15 +78,6 @@ public class PaymentService implements DisposableBean {
 
     private int getDatabaseNodeNumberForSenderId(long senderId) {
         return (int) senderId % 3;
-    }
-
-    private ExecutorService getInsertPaymentExecutor(int databaseNodeNumber) {
-        if (databaseNodeNumber == 0) {
-            return this.db0InsertPaymentExecutor;
-        } else if (databaseNodeNumber == 1) {
-            return this.db1InsertPaymentExecutor;
-        }
-        return this.db2InsertPaymentExecutor;
     }
 
     private PaymentRepository getPaymentRepository(int databaseNodeNumber) {
@@ -107,8 +91,6 @@ public class PaymentService implements DisposableBean {
 
     @Override
     public void destroy() {
-        this.db0InsertPaymentExecutor.shutdown();
-        this.db1InsertPaymentExecutor.shutdown();
-        this.db2InsertPaymentExecutor.shutdown();
+        this.threadPool.shutdown();
     }
 }
