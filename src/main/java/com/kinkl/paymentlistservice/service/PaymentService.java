35eca82be7c632b4f4b697a73238bae1f0c215a5
kinkl <montecarlo507@gmail.com>
2019-07-28 19:34:10 +0500

    Make service perform batch updates

--- src/main/java/com/kinkl/paymentlistservice/service/PaymentService.java
+++ src/main/java/com/kinkl/paymentlistservice/service/PaymentService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class PaymentService {
@@ -28,25 +30,34 @@ public class PaymentService {
 
     public List<PaymentDto> savePayments(List<PaymentDto> payments) {
         List<PaymentDto> unhandledPayments = new ArrayList<>();
-        for (PaymentDto payment : payments) {
-            PaymentRepository paymentRepository = getPaymentRepository(payment.getSenderId());
+        Map<Integer, List<PaymentDto>> groupsOfPayments = payments.stream()
+                .collect(Collectors.groupingBy(e -> getDatabaseNodeNumberForSenderId(e.getSenderId())));
+        for (Map.Entry<Integer, List<PaymentDto>> entry : groupsOfPayments.entrySet()) {
+            PaymentRepository paymentRepository = getPaymentRepository(entry.getKey());
+            List<PaymentDto> paymentListToSaveDto = entry.getValue();
+            List<Payment> paymentListToSave = paymentListToSaveDto.stream() //
+                    .map(payment -> new Payment(payment.getSenderId(), payment.getRecipientId(), payment.getAmount())) //
+                    .collect(Collectors.toList());
             try {
-                paymentRepository.save(new Payment(payment.getSenderId(), payment.getRecipientId(), payment.getAmount()));
+                paymentRepository.saveAll(paymentListToSave);
             } catch (Exception e) {
-                unhandledPayments.add(payment);
+                unhandledPayments.addAll(paymentListToSaveDto);
             }
         }
         return unhandledPayments;
     }
 
     public SenderTotalInfoDto getSenderTotalInfo(long senderId) {
-        PaymentRepository paymentRepository = getPaymentRepository(senderId);
+        PaymentRepository paymentRepository = getPaymentRepository(getDatabaseNodeNumberForSenderId(senderId));
         Long sum = paymentRepository.findSum(senderId);
         return new SenderTotalInfoDto(senderId, sum);
     }
 
-    private PaymentRepository getPaymentRepository(long senderId) {
-        long databaseNodeNumber = senderId % 3;
+    private int getDatabaseNodeNumberForSenderId(long senderId) {
+        return (int) senderId % 3;
+    }
+
+    private PaymentRepository getPaymentRepository(int databaseNodeNumber) {
         if (databaseNodeNumber == 0) {
             return this.db0PaymentRepository;
         } else if (databaseNodeNumber == 1) {
